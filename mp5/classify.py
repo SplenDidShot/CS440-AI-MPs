bd838d615347d7ee061a0ee556dd2941a06b6aa6
James Hsu <chsu41@illinois.edu>
2020-11-04 00:34:24 -0600

    mp5 done

--- mp5/classify.py
+++ mp5/classify.py
@@ -33,16 +33,77 @@ return - a list containing predicted labels for dev_set
 
 import numpy as np
 
+
 def trainPerceptron(train_set, train_labels, learning_rate, max_iter):
     # TODO: Write your code here
     # return the trained weight and bias parameters
-    return W, b
+    weights = np.zeros_like(np.arange(len(train_set[0]), dtype=float))
+    bias = 0.0
+    for _ in range(max_iter):
+        for i in range(len(train_set)):
+            cur_image = np.array(train_set[i])
+            activation_function = np.dot(weights, cur_image) + bias
+            prediction = np.sign(activation_function)
+            if prediction <= 0:
+                prediction = 0
+            else:
+                prediction = 1
 
-def classifyPerceptron(train_set, train_labels, dev_set, learning_rate, max_iter):
+            if prediction != train_labels[i]:
+                if train_labels[i] == 0:
+                    y = -1
+                else:
+                    y = 1
+                weights += learning_rate*y*cur_image
+                bias += learning_rate*y
+
+    return weights, bias
+
+
+def classifyPerceptron(train_set, train_labels, dev_set, learning_rate, max_iter=20):
     # TODO: Write your code here
     # Train perceptron model and return predicted labels of development set
-    return []
+    trained_weights, trained_bias = trainPerceptron(train_set, train_labels, learning_rate, max_iter)
+    result_labels = []
+    for i in range(len(dev_set)):
+        cur_image = np.array(dev_set[i])
+        activation_function = np.dot(trained_weights, cur_image) + trained_bias
+        prediction = np.sign(activation_function)
+
+        if prediction <= 0:
+            prediction = 0
+        else:
+            prediction = 1
+
+        result_labels.append(prediction)
+
+    return result_labels
+
 
 def classifyKNN(train_set, train_labels, dev_set, k):
     # TODO: Write your code here
-    return []
+    result_labels = []
+    from math import sqrt
+    for i in range(len(dev_set)):
+        cur_image = np.array(dev_set[i])
+        distances = []
+        for j in range(len(train_set)):
+            train_image = np.array(train_set[j])
+            dist = np.linalg.norm(cur_image-train_image)
+            # if dist<20:
+            distances.append((dist, train_labels[j]))
+        distances.sort()
+        denom = 0
+        label_sum = 0
+        for n in range(len(distances)):
+            label_sum += distances[n][1]
+            denom += 1
+            if denom==k:
+                break
+        label_sum /= denom
+        if label_sum > 0.5:
+            result_labels.append(1)
+        else:
+            result_labels.append(0)
+
+    return result_labels
